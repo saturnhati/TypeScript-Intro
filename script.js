af7070fdf6fdcbaf62f60596e1c6cc17ee925202
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-10-24 15:55:32 +0200

    fixed removing a useless if and added console logs

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 let FirstPlayer = 13;
 let SecondPlayer = 24;
 let RandomNumb = Math.floor(Math.random() * (100 - 1) + 1);
-console.log(RandomNumb);
+console.log(`Il numero casuale è ${RandomNumb}`);
+console.log(`Il giocatore 1 ha scelto ${FirstPlayer}`);
+console.log(`Il giocatore 2 ha scelto ${SecondPlayer}`);
 function checkNumb() {
     if (FirstPlayer === RandomNumb) {
         console.log("Il giocatore 1 ha indovinato");
@@ -10,13 +12,11 @@ function checkNumb() {
     if (SecondPlayer === RandomNumb) {
         console.log("Il giocatore 2 ha indovinato");
     }
+    else if (Math.abs(RandomNumb - FirstPlayer) < Math.abs(RandomNumb - SecondPlayer)) {
+        console.log("Nessuno dei due ha indovinato, ma il Giocatore 1 si è avvicinato di più");
+    }
     else {
-        if (Math.abs(RandomNumb - FirstPlayer) < Math.abs(RandomNumb - SecondPlayer)) {
-            console.log("Nessuno dei due ha indovinato, ma il Giocatore 1 si è avvicinato di più");
-        }
-        else {
-            console.log("Nessuno dei due ha indovinato, ma il Giocatore 2 si è avvicinato di più");
-        }
+        console.log("Nessuno dei due ha indovinato, ma il Giocatore 2 si è avvicinato di più");
     }
 }
 checkNumb();
--- script.ts
+++ script.ts
@@ -2,22 +2,22 @@ let FirstPlayer: number = 13;
 let SecondPlayer: number = 24;
 
 let RandomNumb: number = Math.floor(Math.random() * (100 - 1) + 1);
-console.log(RandomNumb)
+console.log(`Il numero casuale è ${RandomNumb}`);
+
+console.log(`Il giocatore 1 ha scelto ${FirstPlayer}`);
+console.log(`Il giocatore 2 ha scelto ${SecondPlayer}`);
 
 function checkNumb() {
-    if (FirstPlayer === RandomNumb) {
-            console.log("Il giocatore 1 ha indovinato")
-        }
-    if (SecondPlayer === RandomNumb){
-            console.log("Il giocatore 2 ha indovinato")
-        }
-    else {
-        if (Math.abs(RandomNumb - FirstPlayer) < Math.abs(RandomNumb - SecondPlayer)){
-            console.log("Nessuno dei due ha indovinato, ma il Giocatore 1 si è avvicinato di più")
-        } else {
-            console.log("Nessuno dei due ha indovinato, ma il Giocatore 2 si è avvicinato di più")
-        }
-    }
+  if (FirstPlayer === RandomNumb) {
+    console.log("Il giocatore 1 ha indovinato");
+  }
+  if (SecondPlayer === RandomNumb) {
+    console.log("Il giocatore 2 ha indovinato");
+  } else if (Math.abs(RandomNumb - FirstPlayer) < Math.abs(RandomNumb - SecondPlayer)) {
+    console.log("Nessuno dei due ha indovinato, ma il Giocatore 1 si è avvicinato di più");
+  } else {
+    console.log("Nessuno dei due ha indovinato, ma il Giocatore 2 si è avvicinato di più");
+  }
 }
 
-checkNumb()
+checkNumb();
