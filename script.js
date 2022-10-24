922b9657eb4e7d16a4b6dea56f928b99f960b7c2
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-10-24 15:24:20 +0200

    finished exercise

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+let FirstPlayer = 13;
+let SecondPlayer = 24;
+let RandomNumb = Math.floor(Math.random() * (100 - 1) + 1);
+console.log(RandomNumb);
+function checkNumb() {
+    if (FirstPlayer === RandomNumb) {
+        console.log("Il giocatore 1 ha indovinato");
+    }
+    if (SecondPlayer === RandomNumb) {
+        console.log("Il giocatore 2 ha indovinato");
+    }
+    else {
+        if (Math.abs(RandomNumb - FirstPlayer) < Math.abs(RandomNumb - SecondPlayer)) {
+            console.log("Nessuno dei due ha indovinato, ma il Giocatore 1 si è avvicinato di più");
+        }
+        else {
+            console.log("Nessuno dei due ha indovinato, ma il Giocatore 2 si è avvicinato di più");
+        }
+    }
+}
+checkNumb();
